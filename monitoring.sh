0d2870f3f1a2071092a525c0c830bb3679b7d1a2
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2022-10-14 23:33:16 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -3,7 +3,7 @@
 wall "  #Architecture: `uname -srvmo`
         #CPU physical : `grep 'physical id' /proc/cpuinfo | wc -l`
         #vCPU : `grep 'processor' /proc/cpuinfo | wc -l`
-        #Memory Usage: `free -h | grep Mem | awk '{print $3}'`/`free -h | grep Mem | awk '{print $2}'` `free | grep Mem | awk '{printf("(%.2f%%)"), $2 / ($3 * 100)}'`
+        #Memory Usage: `free --mega | grep Mem | awk '{print $3}'`/`free --mega | grep Mem | awk '{print $2}'` `free --mega | grep Mem | awk '{printf("(%.2f%%)"), $2 / ($3 * 100)}'`
         #Disk Usage: `df -h --total | grep 'total' | awk '{print $3}'`/`df -h --total | grep 'total' | awk '{print $2}'` `df -h --total | grep 'total' | awk '{print ($5)}'`
         #CPU load: `top -bn1 | grep '^%Cpu' | awk '{printf("%.1f%%"), $2 + $4}'`
         #Last boot: `who -b | awk '{print $3 " " $4}'`
