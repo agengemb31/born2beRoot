714ce96c4cb3174db432819096c1158b4901294f
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2022-10-14 19:32:31 +0200

    Create monitoring.sh

---
+++ monitoring.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wall "  #Architecture: `uname -srvmo`
+        #CPU physical : `grep 'physical id' /proc/cpuinfo | wc -l`
+        #vCPU : `grep 'processor' /proc/cpuinfo | wc -l`
+        #Memory Usage: `free -h | grep Mem | awk '{print $3}'`/`free -h | grep Mem | awk '{print $2}'` `free | grep Mem | awk '{printf("%.2f%%"), $2 / $3 * 100}'`
+        #Disk Usage: `df -h --total | grep 'total' | awk '{print $3}'`/`df -h --total | grep 'total' | awk '{print $2}'` `df -h --total | grep 'total' | awk '{print $5}'`
+        #CPU load:
+        #Last boot: `who -b | awk '{print $3 " " $4}`
+        #LVM use:
+        #Connections TCP : `grep 'TCP' /proc/net/sockstat | awk '{print $3}'` ESTABLISHED
+        #User log: `who | wc -l`
+        #Network: IP `hostname -I | awk '{print $1}'` (`ip link | grep 'link/ether' | awk '{print $2}'`)
+        #Sudo : `grep 'COMMAND' /var/log/sudo/sudo.log | wc -l`"
