e5b187e54f685bb907be19bd84e69ba6b1066dd5
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2022-10-29 19:43:37 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -4,7 +4,7 @@ wall "  #Architecture: `uname -srvmo`
         #CPU physical : `grep 'physical id' /proc/cpuinfo | wc -l`
         #vCPU : `grep 'processor' /proc/cpuinfo | wc -l`
         #Memory Usage: `free --mega | grep Mem | awk '{print $3}'`/`free --mega | grep Mem | awk '{print $2}'` `free --mega | grep Mem | awk '{printf("(%.2f%%)"), $2 / ($3 * 100)}'`
-        #Disk Usage: `df -h --total | grep 'total' | awk '{print $3}'`/`df -h --total | grep 'total' | awk '{print $2}'` `df -h --total | grep 'total' | awk '{print ($5)}'`
+        #Disk Usage: `df -h --total | grep 'total' | awk '{print $3}'`/`df -h --total | grep 'total' | awk '{print $2}'` `df -h --total | grep 'total' | awk '{print ("($5)"}'`
         #CPU load: `top -bn1 | grep '^%Cpu' | awk '{printf("%.1f%%"), $2 + $4}'`
         #Last boot: `who -b | awk '{print $3 " " $4}'`
         #LVM use: `if [lsblk | grep 'lvm' | wc -l]; then echo 'no'; else echo 'yes'; fi`
