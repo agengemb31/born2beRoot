5db0a6f8ede57eee70e7bb825d0620dd8b67782a
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2022-11-02 18:49:37 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -3,7 +3,16 @@
 Archi=`uname -srvmo`
 pCPU=`grep 'physical id' /proc/cpuinfo | wc -l`
 vCPU=`grep 'processor' /proc/cpuinfo | wc -l`
-
+memoryUses=`free --mega | grep Mem | awk '{print $3}'`
+totalMemory=`free --mega | grep Mem | awk '{print $2}'`
+memoryPercentage=`free --mega | grep Mem | awk '{printf("(%.2f%%)"), $2 / ($3 * 100)}'`
+diskUses=`df -h --total | grep 'total' | awk '{print $3}'`
+totalDisk=`df -h --total | grep 'total' | awk '{print $2}'`
+diskPercentage=`df -h --total | grep 'total' | awk '{print $5}'`
+cpuLoad=`top -bn1 | grep '^%Cpu' | awk '{printf("%.1f%%"), $2 + $4}'`
+lastBootDate=`who -b | awk '{print $3 " " $4}'`
+isLvmUse=`if [lsblk | grep 'lvm' | wc -l]; then echo 'no'; else echo 'yes'; fi`
+nbTcp=`grep 'TCP' /proc/net/sockstat | awk '{print $3}'`
 nbUserLog=`who | wc -l`
 ipAdress=`hostname -I | awk '{print $1}'`
 macAdress=`ip link | grep 'link/ether' | awk '{print $2}'`
@@ -12,12 +21,12 @@ nbSudo=`grep 'COMMAND' /var/log/sudo/sudo.log | wc -l`
 wall "  #Architecture: $Archi
         #CPU physical : $pCPU
         #vCPU : $vCPU
-        #Memory Usage: `free --mega | grep Mem | awk '{print $3}'`/`free --mega | grep Mem | awk '{print $2}'` `free --mega | grep Mem | awk '{printf("(%.2f%%)"), $2 / ($3 * 100)}'`
-        #Disk Usage: `df -h --total | grep 'total' | awk '{print $3}'`/`df -h --total | grep 'total' | awk '{print $2}'` `df -h --total | grep 'total' | awk '{print $5}'`
-        #CPU load: `top -bn1 | grep '^%Cpu' | awk '{printf("%.1f%%"), $2 + $4}'`
-        #Last boot: `who -b | awk '{print $3 " " $4}'`
-        #LVM use: `if [lsblk | grep 'lvm' | wc -l]; then echo 'no'; else echo 'yes'; fi`
-        #Connections TCP : `grep 'TCP' /proc/net/sockstat | awk '{print $3}'` ESTABLISHED
+        #Memory Usage: $memoryUses/$totalMemory $memoryPercentage
+        #Disk Usage: $diskUses/$totalDisk ($diskPercentage)
+        #CPU load: $cpuLoad
+        #Last boot: $lastBootDate
+        #LVM use: $isLvmUse
+        #Connections TCP : $nbTcp ESTABLISHED
         #User log: $nbUserLog
         #Network: IP $ipAdress ($macAdress)
         #Sudo : $nbSudo cmd"
